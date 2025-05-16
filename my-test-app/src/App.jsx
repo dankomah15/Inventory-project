d24f3c5bf33e14bc06246d9b688469de1f1d0fc5
Ankomah Daniel <[ankomahdaniel995@gmail.com]>
2025-05-16 17:38:58 +0000

    my first commit

--- my-test-app/src/App.jsx
+++ my-test-app/src/App.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import logo from "./assets/new.png";
-// import Logo from "./Components/Logo";
-// import Phone from "./Components/Login";
-// import phone from "./assets/new.png";
-// import Belt from "./Components/Signin";
-// import belt from "./assets/new.png";
-// import Profile from "./Components/Market";
-// import profile from "./assets/new.png";
-// import Purchase from "./Components/Purchase";
-// function App(){
-//   const picture = { logo };
-//   const image = { phone: phone };
-//   const seed = { belt: belt };
-//   const shop = { profile: profile };
-
-//   return(
-//     <>
-//        <Logo {...picture} />
-//        <Phone {...image} />
-//        <Belt {...seed} />
-//       <Profile {...shop} />
-//       <Purchase />
-//     </>
-//   )
-
-  
-
-// }
-
-// export default App;
-
 import Form from "./Components/Table";
 import React from "react";
 import logo from './assets/new.png'; 
@@ -39,16 +7,20 @@ import StockPurchase from "./Components/Purchase";
 
 
 
+
 function App() {
+  const phone = {
+    logo: logo,
+  }
   return (
     <>
     <div>
       <Logo image={logo}/>
       <RegisterForm image={logo}/>
       <Form image={logo} />
-      
+    
       <StockPurchase/>
-      
+       
 
     </div>
     </>
